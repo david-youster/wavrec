8d4cd498cf67aec3f5d2c73c0971f7e0bfdc78c0
David Youster <davidyouster@gmail.com>
2024-09-16 22:15:11 +0100

    Add tests for audio module

--- src/audio.rs
+++ src/audio.rs
@@ -65,3 +65,132 @@ pub trait AudioLoopback {
     fn init(&self) -> Nothing;
     fn capture(&self, transmitter: Sender<Vec<u8>>) -> Nothing;
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::format;
+
+    use super::*;
+
+    const DEFAULT_SAMPLE_RATE: u32 = 44100;
+    const DEFAULT_NUM_CHANNELS: u8 = 2;
+
+    #[test]
+    fn sample_format_returns_correct_bit_depth_values() {
+        assert_eq!(SampleFormat::Int16.bit_depth(), 16);
+        assert_eq!(SampleFormat::Int24.bit_depth(), 24);
+        assert_eq!(SampleFormat::Int32.bit_depth(), 32);
+        assert_eq!(SampleFormat::Float32.bit_depth(), 32);
+    }
+
+    #[test]
+    fn audio_format_info_returns_correct_bit_depth_values() {
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Int16,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.bit_depth(), 16);
+
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Int24,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.bit_depth(), 24);
+
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Int32,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.bit_depth(), 32);
+
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Float32,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.bit_depth(), 32);
+    }
+
+    #[test]
+    fn audio_format_info_returns_correct_type_format() {
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Int16,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.type_format_header(), 0x0001);
+
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Int24,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.type_format_header(), 0x0001);
+
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Int32,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.type_format_header(), 0x0001);
+
+        let format_info = create_audio_format_info(
+            DEFAULT_SAMPLE_RATE,
+            SampleFormat::Float32,
+            DEFAULT_NUM_CHANNELS,
+        );
+        assert_eq!(format_info.type_format_header(), 0x0003);
+    }
+
+    #[test]
+    fn audio_format_info_bytes_per_second_returns_correct_value() {
+        validate_bytes_per_second(44100, SampleFormat::Int16, DEFAULT_NUM_CHANNELS);
+        validate_bytes_per_second(44100, SampleFormat::Int24, DEFAULT_NUM_CHANNELS);
+        validate_bytes_per_second(48000, SampleFormat::Int16, DEFAULT_NUM_CHANNELS);
+        validate_bytes_per_second(48000, SampleFormat::Int24, DEFAULT_NUM_CHANNELS);
+        validate_bytes_per_second(96000, SampleFormat::Int32, DEFAULT_NUM_CHANNELS);
+        validate_bytes_per_second(96000, SampleFormat::Float32, DEFAULT_NUM_CHANNELS);
+    }
+
+    #[test]
+    fn audio_format_info_block_alignment_returns_correct_value() {
+        validate_block_alignment(SampleFormat::Int16, DEFAULT_NUM_CHANNELS);
+        validate_block_alignment(SampleFormat::Int24, DEFAULT_NUM_CHANNELS);
+        validate_block_alignment(SampleFormat::Int32, DEFAULT_NUM_CHANNELS);
+        validate_block_alignment(SampleFormat::Float32, DEFAULT_NUM_CHANNELS);
+
+        validate_block_alignment(SampleFormat::Int16, DEFAULT_NUM_CHANNELS * 2);
+        validate_block_alignment(SampleFormat::Int24, DEFAULT_NUM_CHANNELS * 2);
+        validate_block_alignment(SampleFormat::Int32, DEFAULT_NUM_CHANNELS * 2);
+        validate_block_alignment(SampleFormat::Float32, DEFAULT_NUM_CHANNELS * 2);
+    }
+
+    fn create_audio_format_info(
+        sample_rate: u32,
+        sample_format: SampleFormat,
+        num_channels: u8,
+    ) -> AudioFormatInfo {
+        AudioFormatInfo {
+            sample_rate,
+            num_channels: num_channels as u8,
+            format: sample_format,
+        }
+    }
+
+    fn validate_bytes_per_second(sample_rate: u32, sample_format: SampleFormat, num_channels: u8) {
+        let format_info = create_audio_format_info(sample_rate, sample_format, num_channels);
+        let expected_result =
+            (sample_rate * sample_format.bit_depth() as u32 * num_channels as u32) / 8;
+        assert_eq!(format_info.bytes_per_second(), expected_result);
+    }
+
+    fn validate_block_alignment(sample_format: SampleFormat, num_channels: u8) {
+        let format_info =
+            create_audio_format_info(DEFAULT_SAMPLE_RATE, sample_format, num_channels);
+        let expected_result: u16 = (format_info.bit_depth() as u16 * num_channels as u16) / 8;
+        assert_eq!(format_info.block_alignment(), expected_result);
+    }
+}
